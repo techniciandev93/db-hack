298a6d23b3c74b0ebfc673680b4ea7b4163a617e
anton <anton@adminpc.Dlink>
2023-09-03 12:49:23 +0300

    added fixes

--- scripts.py
+++ scripts.py
@@ -1,15 +1,30 @@
 import os
 import django
 import random
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import MultipleObjectsReturned
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 django.setup()
 
 from datacenter.models import Mark, Chastisement, Schoolkid, Lesson, Commendation
 
+WISHES = ['Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!',
+          'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!',
+          'Сказано здорово – просто и ясно!', 'Ты, как всегда, точен!', 'Очень хороший ответ!', 'Талантливо!',
+          'Ты сегодня прыгнул выше головы!', 'Я поражен!', 'Уже существенно лучше!', 'Потрясающе!',
+          'Замечательно!', 'Прекрасное начало!', 'Так держать!', 'Ты на верном пути!', 'Здорово!',
+          'Это как раз то, что нужно!', 'Я тобой горжусь!', 'С каждым разом у тебя получается всё лучше!',
+          'Мы с тобой не зря поработали!', 'Я вижу, как ты стараешься!', 'Ты растешь над собой!',
+          'Ты многое сделал, я это вижу!', 'Теперь у тебя точно все получится!']
+
 
 def get_child(fio):
-    child = Schoolkid.objects.get(full_name__contains=fio)
+    try:
+        child = get_object_or_404(Schoolkid, full_name__contains=fio)
+    except MultipleObjectsReturned:
+        raise Http404()
     return child
 
 
@@ -26,20 +41,11 @@ def remove_chastisements(fio):
 
 
 def create_commendation(fio, subject):
-    wishes = ['Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!',
-              'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!',
-              'Сказано здорово – просто и ясно!', 'Ты, как всегда, точен!', 'Очень хороший ответ!', 'Талантливо!',
-              'Ты сегодня прыгнул выше головы!', 'Я поражен!', 'Уже существенно лучше!', 'Потрясающе!',
-              'Замечательно!', 'Прекрасное начало!', 'Так держать!', 'Ты на верном пути!', 'Здорово!',
-              'Это как раз то, что нужно!', 'Я тобой горжусь!', 'С каждым разом у тебя получается всё лучше!',
-              'Мы с тобой не зря поработали!', 'Я вижу, как ты стараешься!', 'Ты растешь над собой!',
-              'Ты многое сделал, я это вижу!', 'Теперь у тебя точно все получится!']
-
     child = get_child(fio)
     child_random_lesson = Lesson.objects.filter(year_of_study=child.year_of_study,
                                                 group_letter=child.group_letter,
                                                 subject__title=subject).order_by('?').first()
-    Commendation.objects.create(text=random.choice(wishes),
+    Commendation.objects.create(text=random.choice(WISHES),
                                 created=child_random_lesson.date,
                                 schoolkid=child,
                                 subject=child_random_lesson.subject,
